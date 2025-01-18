09d84a9c3511bb154055ff73235d6d99cd8401c3
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2025-01-19 01:08:24 +0600

    add favicon, little changes of menu

--- main/views.py
+++ main/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
+from django.views.generic.list import ListView
 
 from datetime import datetime
 
+from content.models import Post
 from .utils import NavbarMixin
 # Create your views here.
 
@@ -17,5 +19,6 @@ class IndexView(NavbarMixin,TemplateView):
         context = super().get_context_data(**kwargs)
         context['year'] = datetime.now().year
         context['users'] = User.objects.all()
+        context['posts'] = Post.objects.all()
         return context
     
